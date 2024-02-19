8fb862454b9cf96c5cc768759096f0f5a91da052
Oleg Zaslavskii <Zasoleg1995@gmail.com>
2024-02-19 20:27:13 +0300

    add abstract class FileManager

--- src/FileManager.py
+++ src/FileManager.py
@@ -1,4 +1,20 @@
+import json
+from abc import ABC, abstractmethod
 
 
-def JSONSaver():
-    pass
+class FileManager(ABC):
+    @abstractmethod
+    def add_to_file(self, path, vacancies):
+        pass
+
+    @abstractmethod
+    def read_file(self, path):
+        pass
+
+    @abstractmethod
+    def delete_from_file(self, path):
+        pass
+
+
+
+
