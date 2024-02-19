9d5bb57280a2bff1303e2cdbf45c7d32a49b2664
Oleg Zaslavskii <Zasoleg1995@gmail.com>
2024-02-19 20:31:02 +0300

    added functionality to main

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+from src.HH_API import HeadHunterAPI
+from src.Vacancy import Vacancy
+from src.FileManager import JSONSaver
+from config import PATH_TO_JSON_DATA
+
+
 """
 Стартуем программу с приветствия и запросов
 1) Какие профессии интересуют
@@ -43,3 +49,36 @@ while True:
     except ValueError:
         print("Попробуйте снова, введите число")
         continue
+
+# Создание экземпляра класса для работы с API сайтов с вакансиями
+hh_api = HeadHunterAPI()
+
+# Получение вакансий с hh.ru в формате JSON
+hh_vacancies = hh_api.get_vacancies(search_query)
+
+# Преобразование набора данных из JSON в список объектов
+vacancies_list = Vacancy.cast_to_object_list(hh_vacancies)
+
+# Отбираем только вакансии, подходящие по кодовым словам
+filtered_vacancies = Vacancy.get_filtered_vacancies(vacancies_list, filter_words)
+
+# оставляем только вакансии, подходящие по зарплате
+vacancies_by_salary = Vacancy.get_vacancies_by_salary(filtered_vacancies, salary_from, salary_to)
+
+# сортируем вакансии
+sorted_vacancies = Vacancy.get_sorted_vacancies(vacancies_by_salary)
+
+# оставляем только верхние top_n вакансий
+top_vacancies = Vacancy.get_top_vacancies(sorted_vacancies, top_n)
+
+# Сохранение информации об интересующих вакансиях в файл
+json_saver = JSONSaver()
+json_saver.add_to_file(PATH_TO_JSON_DATA, top_vacancies)
+
+# Выводим пользователю то что он просил
+print("Ниже список вакансий, что вы искали")
+json_saver.read_file(PATH_TO_JSON_DATA)
+print("Найденные вакансии сохранены в файл vacancies.json")
+
+# Прощаемся с пользователем
+print("Благодарим за использование, надеюсь вы нашли работу мечты!")
